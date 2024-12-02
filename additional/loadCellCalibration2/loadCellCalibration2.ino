5e7717fedd176ef515385d70acad6ebe9c709ee7
Victor Statko <v.statko@softarex.com>
2024-12-03 00:22:04 +0300

    fix load cell calibration (2)

--- additional/loadCellCalibration2/loadCellCalibration2.ino
+++ additional/loadCellCalibration2/loadCellCalibration2.ino
@@ -16,18 +16,9 @@ void setup() {
 void loop() {
 
   if (scale.is_ready()) {
-    scale.set_scale();    
-    Serial.println("Tare... remove any weights from the scale.");
-    delay(5000);
-    scale.tare();
-    Serial.println("Tare done...");
-    Serial.print("Place a known weight on the scale...");
-    delay(5000);
-    long reading = scale.get_units(10);
-    Serial.print("Result: ");
-    Serial.println(reading);
-  } 
-  else {
+    Serial.println("Offset");
+    Serial.println(scale.read_average(20));
+  } else {
     Serial.println("HX711 not found.");
   }
   delay(1000);
